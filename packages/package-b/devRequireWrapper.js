1aac40243e3d1391088b4126af6d0b47fc861c2b
aevans04 <evans.aaron@epa.gov>
2023-04-20 19:24:32 -0400

    change structure of dev-require config file and added some docs

--- packages/package-b/devRequireWrapper.js
+++ packages/package-b/devRequireWrapper.js
@@ -4,14 +4,14 @@
 
 //can probably use npm link for this for just comment and uncomment because can't use dev-require for dev-require!
 //if hard coding like this might have to update the path of local dev-require below
-//let devRequireFactory = require('../dev-require');
-let devRequireFactory = require('@aevans04/dev-require');
+let devRequireFactory = require('../dev-require');
+//let devRequireFactory = require('@aevans04/dev-require');
 
 let devRequire = devRequireFactory();
 
 function devRequireWrapper(packageName) {
     //this if we want to just require remote from npm in here instead of inside dev-require
-    let packageInstance = devRequire.require(packageName,{settingName:'alt'}) || require(packageName);
+    let packageInstance = devRequire.require(packageName,{alternateName:'alt1'}) || require(packageName);
 
     return packageInstance
 }
